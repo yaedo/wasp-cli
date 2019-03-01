43b08cd57e657dd802d2fa70f0ee970dcb3a25ba
Cameron Bytheway <bytheway.cameron@gmail.com>
2019-02-28 23:04:21 -0700

    release 0.1.4

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,12 @@ enum Opt {
 
         #[structopt(short = "e", long = "env-file")]
         env_file: Option<String>,
+
+        #[structopt(short = "c", long = "cdn-directory")]
+        cdn_directory: Option<String>,
+
+        #[structopt(short = "P", long = "protected-cdn-directory")]
+        protected_cdn_directory: Option<String>,
     },
 
     #[structopt(name = "deploy")]
@@ -53,12 +59,28 @@ fn main() {
             function,
             port,
             env_file,
-        } => run(module, function, port, env_file),
+            cdn_directory,
+            protected_cdn_directory,
+        } => run(
+            module,
+            function,
+            port,
+            env_file,
+            cdn_directory,
+            protected_cdn_directory,
+        ),
         _ => unimplemented!(),
     }
 }
 
-fn run(module: String, function: String, port: usize, env_file: Option<String>) {
+fn run(
+    module: String,
+    function: String,
+    port: usize,
+    env_file: Option<String>,
+    cdn_directory: Option<String>,
+    protected_cdn_directory: Option<String>,
+) {
     if let Some(file) = env_file {
         dotenv::from_filename(file).expect("Could not load env file");
     }
@@ -66,6 +88,14 @@ fn run(module: String, function: String, port: usize, env_file: Option<String>) 
     set_var("WASP_PLATFORM_FILE", module);
     set_var("WASP_PLATFORM_ENTRY_FUNCTION", function);
     set_var("PORT", port.to_string());
+
+    if let Some(dir) = cdn_directory {
+        set_var("WASP_CDN_DIRECTORY", dir);
+    }
+
+    if let Some(dir) = protected_cdn_directory {
+        set_var("WASP_PROTECTED_CDN_DIRECTORY", dir);
+    }
 
     start();
 }
